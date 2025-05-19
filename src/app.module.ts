5fa0a72a790cd0c8c31592479dcf1ded154db950
manuel <manuelds@outlook.pt>
2025-05-19 19:32:28 +0100

    feat: questions service base

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule, QuestionsModule],
   controllers: [],
   providers: [PrismaService],
 })
---
+++ src/questions/questions.service.ts
@@ -0,0 +1,44 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
+
+@Injectable()
+export class QuestionsService {
+  @Inject()
+  private readonly prisma: PrismaService
+  async create(createQuestionDto: CreateQuestionDto) {
+    const userId = "dskln"
+    return await this.prisma.question.create({
+      data: { ...createQuestionDto, userId }
+
+    })
+  }
+
+  async findAll() {
+    return await this.prisma.question.findMany()
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.question.findUnique({ where: { id: id } })
+  }
+
+  async update(id: string, updateQuestionDto: UpdateQuestionDto) {
+    // const userId = "dskln"
+    return await this.prisma.question.update({
+      where: {
+        id: id,
+      },
+      data: updateQuestionDto
+
+    })
+  }
+
+  async remove(id: string) {
+    return this.prisma.question.delete({
+      where: {
+        id: id
+      }
+    })
+  }
+}
