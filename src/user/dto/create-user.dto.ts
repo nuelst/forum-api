b269f50848c28aaf1d675a38c621b2c050688023
manuel <manuelds@outlook.pt>
2025-05-20 22:26:14 +0100

    feat: add pipes to validate

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+  @MinLength(2)
+  @MaxLength(50)
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  // @IsAlphanumeric()
+  @MinLength(6)
+  @IsNotEmpty()
+  @MaxLength(32)
+  @IsString()
+  password: string;
+}
